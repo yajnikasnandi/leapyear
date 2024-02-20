86f2becb8461d8e4a2e580d1c015f526b5fd97f8
manoj <manumanojks600@gmaail.com>
2024-02-20 15:00:28 +0530

    #

--- leap_year.c
+++ leap_year.c
@@ -1,23 +1,23 @@
 #include <stdio.h>
 int main() {
    int y;
-   printf("Enter a y: ");
+   printf("Enter a year: ");
    scanf("%d", &y);
 
    if (y% 400 == 0) {
-      printf("%d is a leap y.", y);
+      printf("%d is a leap year.", y);
    }
    
    else if (y% 100 == 0) {
-      printf("%d is not a leap y.", y);
+      printf("%d is not a leap year.", y);
    }
 
    else if (y % 4 == 0) {
-      printf("%d is a leap y.", y);
+      printf("%d is a leap year.", y);
    }
    
    else {
-      printf("%d is not a leap y.", y);
+      printf("%d is not a leap year.", y);
    }
 
    return 0;
